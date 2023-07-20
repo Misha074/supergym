8322658bb53d6f07e7f0d7a3608cf56e5b50841c
Misha074 <103427594+Misha074@users.noreply.github.com>
2023-07-21 01:26:34 +0400

    Исправления
    Исправления по табличке, и по ссылке.
    https://docs.google.com/spreadsheets/d/16AdRrD3zcnYbxLwb6zdE4daNV0PNdc8HjHjNG8IgB9Q/edit#gid=991403847
    
    https://gist.github.com/fdkitamory/5344fd4a7341f525f3afda7c7fd267cb

--- source/js/modules/scroll/scroll.js
+++ source/js/modules/scroll/scroll.js
@@ -4,4 +4,3 @@ let buyTicketLink = document.getElementById('buy-ticket');
 buyTicketLink.addEventListener('click', () => {
   tickets.scrollIntoView({behavior: 'smooth'}, {block: 'center', inline: 'center'});
 });
-
