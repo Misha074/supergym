93ed6a1c3bce0fc4b1fb1bccdd4e2f5e6bc48f45
Misha074 <103427594+Misha074@users.noreply.github.com>
2023-07-23 01:30:01 +0400

    Исправления
    Ещё исправления

--- source/js/main.js
+++ source/js/main.js
@@ -1,11 +1,10 @@
 import {iosVhFix} from './utils/ios-vh-fix';
-// import {Form} from './modules/form-validate/form';
 import './modules/scroll/scroll';
 import './modules/youtube-video/youtube-video';
-import './modules/tabs/index';
-import './modules/swiper-coaches/swiper-coaches';
-import './modules/accordion/index';
-import './modules/swiper-reviews/swiper-reviews';
+import {initTabs} from './modules/tabs/init-tabs';
+import {initCoachesSwiper} from './modules/swiper-coaches/swiper-coaches';
+import {initAccordions} from './modules/accordion/init-accordion';
+import {initReviewsSwiper} from './modules/swiper-reviews/swiper-reviews';
 // ---------------------------------
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -20,9 +19,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  // window.addEventListener('load', () => {
-  //   const form = new Form();
-  //   window.form = form;
-  //   form.init();
-  // });
+  window.addEventListener('load', () => {
+    initTabs();
+    initCoachesSwiper();
+    initAccordions();
+    initReviewsSwiper();
+  });
 });
